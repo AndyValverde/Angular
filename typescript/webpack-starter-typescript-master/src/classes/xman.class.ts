a7cb807a7a911ae9b3dccbe2e8c52ee9e8e61640
AndyValverde <lupy29@hotmail.com>
2020-12-30 09:27:44 -0600

    Decoradores ejemplo terminado y comentado

--- typescript/webpack-starter-typescript-master/src/classes/xman.class.ts
+++ typescript/webpack-starter-typescript-master/src/classes/xman.class.ts
@@ -1,3 +1,7 @@
+ function imprimirClass(constructorClass : Function){// es una funcion que recibe de argumento una funcion (este va a ser el decorador)
+    console.log(constructorClass);
+ }
+ @imprimirClass// aqui estamos llamando a la funcion que le agrega esta funcionalidad a la clase
  export class Xman {// esta clase si puede ser importada
     constructor(
         public nombre : string,
--- typescript/webpack-starter-typescript-master/src/index.ts
+++ typescript/webpack-starter-typescript-master/src/index.ts
@@ -1,3 +1,4 @@
 import {Xman} from './classes/xman.class'; //si hubiera mas clases podriamos importarlas con export en este archivo
 const magneto = new Xman("prueba","prueba");
-magneto.imprimir(); //utiliza el metodo imprimir de la clase importada
+
+//magneto.imprimir(); //utiliza el metodo imprimir de la clase importada
