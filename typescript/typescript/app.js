80dd8033d027839a9b7ecb52b82b2e61849c39f6
AndyValverde <lupy29@hotmail.com>
2020-12-30 10:51:30 -0600

    Examen practico

--- typescript/typescript/app.js
+++ typescript/typescript/app.js
@@ -1,2 +1,43 @@
 (() => {
+    // Uso de Let y Const
+    const nombre = 'Ricardo Tapia';
+    const edad = 23;
+    const PERSONAJE = {
+        nombre: nombre,
+        edad: edad
+    };
+    const batman = {
+        nombre: 'Bruno Díaz',
+        artesMarciales: ['Karate', 'Aikido', 'Wing Chun', 'Jiu-Jitsu']
+    };
+    // Convertir esta funcion a una funcion de flecha
+    const resultadoDoble = (a, b) => (a + b) * 2;
+    // Función con parametros obligatorios, opcionales y por defecto
+    // donde NOMBRE = obligatorio
+    //       PODER  = opcional
+    //       ARMA   = por defecto = 'arco'
+    const getAvenger = (nombre, poder, arma = 'arco') => {
+        if (poder) {
+            return `${nombre} tiene el poder de ${poder} y un arma ${arma}`;
+        }
+        else {
+            return `${nombre} tiene un arma ${arma}`;
+        }
+    };
+    // Cree una clase que permita manejar la siguiente estructura
+    // La clase se debe de llamar rectangulo,
+    // debe de tener dos propiedades:
+    //   * base
+    //   * altura
+    // También un método que calcule el área  =  base * altura,
+    // ese método debe de retornar un numero.
+    class rectangulo {
+        constructor(base, altura) {
+            this.base = base;
+            this.altura = altura;
+        }
+        calcularElArea() {
+            return this.base * this.altura;
+        }
+    }
 })();
--- typescript/typescript/app.ts
+++ typescript/typescript/app.ts
@@ -1,3 +1,54 @@
 (()=>{
-    
-})();  
+
+  // Uso de Let y Const
+  const nombre = 'Ricardo Tapia';
+  const edad = 23;
+
+  const PERSONAJE = {
+    nombre: nombre,
+    edad: edad
+  };
+
+
+  // Cree una interfaz que sirva para validar el siguiente objeto
+  interface superheroe {
+    nombre : string;
+    artesMarciales : string[];
+  }
+  const batman : superheroe= {
+    nombre: 'Bruno Díaz',
+    artesMarciales: ['Karate','Aikido','Wing Chun','Jiu-Jitsu']
+  }
+
+  // Convertir esta funcion a una funcion de flecha
+  const resultadoDoble = ( a : number , b : number) : number =>  (a + b) * 2
+  
+
+  // Función con parametros obligatorios, opcionales y por defecto
+  // donde NOMBRE = obligatorio
+  //       PODER  = opcional
+  //       ARMA   = por defecto = 'arco'
+  const getAvenger = ( nombre: string, poder? : string, arma : string = 'arco') : string => {
+    if( poder ){
+      return `${nombre} tiene el poder de ${poder} y un arma ${arma}`;
+    }else{
+      return `${nombre} tiene un arma ${arma}`
+    }
+  };
+
+  // Cree una clase que permita manejar la siguiente estructura
+  // La clase se debe de llamar rectangulo,
+  // debe de tener dos propiedades:
+  //   * base
+  //   * altura
+  // También un método que calcule el área  =  base * altura,
+  // ese método debe de retornar un numero.
+  class rectangulo {
+      constructor(public base : number, public altura : number){}
+      calcularElArea() : number{
+        return this.base * this.altura;
+      }
+  }
+
+
+})();
