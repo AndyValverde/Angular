bcfb92532b9d881fa58a212ec7e48e27ea0a8573
AndyValverde <lupy29@hotmail.com>
2020-12-30 10:14:03 -0600

    Cambie el archivo de configuraciones para que acepte promesas (es6) y hice dos ejercicios de tipado de retorno de funciones

--- typescript/typescript/app.js
+++ typescript/typescript/app.js
@@ -1,2 +1,24 @@
-(function () {
+(() => {
+    const sumar = (a, b) => {
+        return a + b;
+    };
+    const restar = (a, b) => {
+        return a - b;
+    };
+    const desconocido = () => {
+        return new Promise((resort, reject) => {
+            resort('Hola');
+        });
+    };
+    //desconocido. no aparece porque necesitas "()" para que sea funcion
+    //si imprime en mayusculas aunque lo marque como incorrecto
+    desconocido().then(x => console.log(x.toUpperCase())); // no reconoce el toUpperCase porque la promes (linea 8) no nos dice que es un string
+    const conocido = () => {
+        return new Promise((resort, reject) => {
+            resort('hello');
+            reject(2);
+        });
+    };
+    conocido().then(x => console.log(x.toUpperCase)).catch(x => console.log(x)); // sino le pones los "()" al toUpperCase no va imprimir el resort
+    conocido().then(x => console.log(x.toUpperCase())).catch(x => console.log(x));
 })();
--- typescript/typescript/app.ts
+++ typescript/typescript/app.ts
@@ -1,7 +1,27 @@
-
 (()=>{
+    const sumar = (a : number, b : number) => {// sin tipado de retorno
+        return a + b;
+    }
+    const restar = (a : number, b : number) : number => { //con tipado de retorno
+        return a - b;
+    }
+    const desconocido = () =>{// no defenimos el tipo de retorno por eso Prmise<unkned>
+        return new Promise((resort, reject) =>{
+            resort('Hola');
+        });
+    }
+    //desconocido. no aparece porque necesitas "()" para que sea funcion
+    //si imprime en mayusculas aunque lo marque como incorrecto
+    desconocido().then(x => console.log(x.toUpperCase())); // no reconoce el toUpperCase porque la promes (linea 8) no nos dice que es un string
+
+
+    const conocido = () : Promise<string> => {// el tipo string solo afecta al resort no al reject
+        return  new Promise((resort , reject) => {
+            resort('hello');
+            reject(2);
+        });
+    }
+    conocido().then(x => console.log(x.toUpperCase)).catch(x => console.log(x));// sino le pones los "()" al toUpperCase no va imprimir el resort
+    conocido().then(x => console.log(x.toUpperCase())).catch(x => console.log(x));
 
 })();  
-
-
-
