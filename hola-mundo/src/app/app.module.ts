8d001c5f321f03354c403885db8803526d8b5117
AndyValverde <lupy29@hotmail.com>
2020-12-31 10:48:15 -0600

    Ejercicio propio component body

--- hola-mundo/src/app/app.module.ts
+++ hola-mundo/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HeaderCom} from './components/header/header.component';
+import { bodyComp } from './components/body/body.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderCom
+    HeaderCom,
+    bodyComp
   ],
   imports: [
     BrowserModule
