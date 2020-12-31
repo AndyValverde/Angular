e1f942525491cb6c085c51ad2af33c1bfc17473c
AndyValverde <lupy29@hotmail.com>
2020-12-31 11:55:15 -0600

    Generacion component automatico (ng g c components/footer)

--- hola-mundo/src/app/app.module.ts
+++ hola-mundo/src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import {HeaderCom} from './components/header/header.component';
-import { bodyComp } from './components/body/body.component'
+import { bodyComp } from './components/body/body.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderCom,
-    bodyComp
+    bodyComp,
+    FooterComponent
   ],
   imports: [
     BrowserModule
