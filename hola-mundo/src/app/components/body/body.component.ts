b8f6eb70cfe4c24211b056a95d19d3feb9041662
AndyValverde <lupy29@hotmail.com>
2021-01-04 09:57:15 -0600

    directivas *niIf y *ngFor primer programa con angular

--- hola-mundo/src/app/components/body/body.component.ts
+++ hola-mundo/src/app/components/body/body.component.ts
@@ -4,4 +4,11 @@ import {Component} from '@angular/core'
     selector : 'app-body',
     templateUrl: 'body.component.html'
 })
-export class bodyComp{}
+export class bodyComp{
+    mostrar : boolean = true;
+    frase : any = {
+        mensaje : `Un gran poder conlleva una gran responsabilidad`,
+        autor : `Ben parker`
+    };
+    super : string [] = ["spiderman", "batman", "superman"];
+}
