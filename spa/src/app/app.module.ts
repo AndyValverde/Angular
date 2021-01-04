72e55650efa52feee5d09ffed65a90d59db5b64f
AndyValverde <lupy29@hotmail.com>
2021-01-04 14:44:34 -0600

    archivos necesarios antes de comenzar las rutas

--- spa/src/app/app.module.ts
+++ spa/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './src/components/home/home.component';
 import { NavbarComponent } from './src/components/share/navbar/navbar.component';
+import { AboutComponent } from './src/components/about/about.component';
+import { HeroesComponent } from './src/components/heroes/heroes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule
