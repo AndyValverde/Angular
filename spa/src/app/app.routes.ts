cfef91de77cdf0cf75a80c6ad7bd1f92d3e11ded
AndyValverde <lupy29@hotmail.com>
2021-01-12 10:30:39 -0600

    Completar clase de rutas y agregar comentarios y la clase animated de estilos css

--- spa/src/app/app.routes.ts
+++ spa/src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import {RouterModule,Routes} from '@angular/router';
+//importamos componentes para poder utilizarlos
 import {HomeComponent} from 'src/app/components/home/home.component';
-import {AboutComponent} from 'src/app/components/about/about.component'
-const APP_ROUTES: Routes = [
+import {AboutComponent} from 'src/app/components/about/about.component';
+import {HeroesComponent} from 'src/app/components/heroes/heroes.component';
+const APP_ROUTES: Routes = [ //estas rutas seran las que podemos utilizar
  {path : 'home', component : HomeComponent},
  {path : 'about', component : AboutComponent},
- {path : '**' , pathMatch: 'full', redirectTo : 'home'}
+ {path : 'heroes', component : HeroesComponent},
+ {path : '**' , pathMatch: 'full', redirectTo : 'home'}///por defecto
 ];
+
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
