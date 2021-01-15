5e7cb2025c5e4472d2cd330c5943a025d6899792
AndyValverde <lupy29@hotmail.com>
2021-01-15 12:33:20 -0600

    mi primer pipe Capitalizado completo volver a mayusculas la primera letra de cada pala bra o solo la primera letra

--- pipes/src/app/pipes/capitalizado.pipe.ts
+++ pipes/src/app/pipes/capitalizado.pipe.ts
@@ -5,8 +5,27 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CapitalizadoPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string, todo:boolean = true): string {
+    //vuelve todo a minuscula
+    value = value.toLowerCase();
+    //separa el string en un arreglo cada vez que aparezca lo que esta " " 
+    let variable  = value.split(" ");
+    //si la variable es true
+    if (todo){
+      //pone en la variable el resultado de map - map por cada elemento del arreglo realiza una función y vuelve a poner el elemento trasformado en la misma posisicón
+      variable = variable.map(resultado => {
+        //en la posición cero de cada elemento (cada elemento es resultado) pone en mayuscula y le agrega el mismo elemento pero a partir de la posicón 1
+        return resultado[0].toUpperCase() + resultado.substr(1);
+      });
+    }
+    // si es false
+    else{
+      //en la posicion cero del srreglo(variable[0]) pone lo en la primeraposición del primer elemento en mayuscula (variable[0][0].toUpperCase() )
+      //y le adjunta lo que tiene el primer elemento a partir de la posición 1 (variable[0].substr(1)) 
+      return variable[0]= variable[0][0].toUpperCase() + variable[0].substr(1);
+    }
+    //junta cada valor del arreglo cada elemento es unido con lo que esta entre " " podria ir una letra
+    return variable.join(" ");
   }
 
 }
